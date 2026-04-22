ded4c40a32ca82f95b869c1c6925354579fac393
anngreen <anngreen@qq.com>
2026-04-22 08:49:14 +0800

    style

---
+++ style/sass/code/src/readme.md
@@ -0,0 +1,17 @@
+- 注释 `./comments.scss`
+- 嵌套 `./nesting.scss`
+- 插值 `./interpolation.scss`
+- 父选择器 `./parent-selector.scss`
+- 占位选择器 `./placeholder-selectors.scss`
+- 变量 `./variables.scss`
+
+
+- 运算符 `./operators.scss`
+
+
+
+
+
+```
+npx sass .\parent-selector.scss style.css
+```
---
+++ style/unocss/code/unocss-vue-demo/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <!-- <HelloWorld /> -->
+
+  <div class="text-4xl font-600 font-italic underline lh-tight">
+    文字
+  </div>
+
+  <div class="border-2 rd-2xl border-dashed inline-block lh-10 text-4xl lh-20">
+    文字
+  </div>
+
+  <div class="p-x-4 py-8 mb-2 mt-3 inline-block border-2">
+    边距
+  </div>
+
+  <div class="w-40 h-30 border-2 py-2">
+    <div class="h-full w-screen border-dashed border-1">宽高</div>
+  </div>
+</template>
